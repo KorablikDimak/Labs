798d1fe1af83f81f0d6b601555fd7e4751ca14be
korab <korablikdimak@gmail.com>
2023-09-13 23:37:23 +0300

    3

---
+++ lab1/3.py
@@ -0,0 +1,18 @@
+def fib(previous, current, target):
+    if target == 0:
+        return True
+    elif current == target:
+        return True
+    elif current > target:
+        return False
+    else:
+        return fib(current, current + previous, target)
+
+
+while True:
+    print("Введите число на проверку принадлежности к числам Фибоначчи:")
+    try:
+        target = int(input())
+        print(fib(0, 1, target))
+    except ValueError:
+        print("Ошибка ввода")
