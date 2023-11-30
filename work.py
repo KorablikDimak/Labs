44745f595dd22b108e56e1b80f7f4e176fa8a598
korab <korablikdimak@gmail.com>
2023-11-30 14:55:45 +0300

    lab5

---
+++ work.py
@@ -0,0 +1,15 @@
+def generate_combinations(string: str) -> set[str]:
+    combinations = set()
+    if ' ' in string:
+        combinations.add('')
+    if len(string) > 1:
+        for i in range(len(string)):
+            for char in generate_combinations(string[:i] + string[i + 1:]):
+                combinations.add(char)
+                combinations.add(string[i] + char)
+    combinations.add(string)
+    return combinations
+
+input_string = input("Введите строку: ")
+result = generate_combinations(input_string)
+print(result)
